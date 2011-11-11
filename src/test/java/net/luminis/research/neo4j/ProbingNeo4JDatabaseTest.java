9730db9728f52d56cea6285c3af1f6ac694e16e9
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-11-11 09:21:34 +0100

    Test setup improved
    We create one database per test class.

--- src/test/java/net/luminis/research/neo4j/ProbingNeo4JDatabaseTest.java
+++ src/test/java/net/luminis/research/neo4j/ProbingNeo4JDatabaseTest.java
@@ -1,14 +1,28 @@
 package net.luminis.research.neo4j;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 
 public class ProbingNeo4JDatabaseTest {
+	private static GraphDatabaseService graphDb;
+
+	@BeforeClass
+	public static void createGraphDatabase() {
+		graphDb = new EmbeddedGraphDatabase("src/test/resources/graphdbs/starting");
+	}
 
 	@Test
-	public void setupDatabase() {
-		GraphDatabaseService graphDb = new EmbeddedGraphDatabase("src/test/resources/graphdbs/starting");
+	public void graphDbShouldBeInitialized() {
+		assertNotNull(graphDb);
+	}
+
+	@AfterClass
+	public static void setupDatabase() {
 		graphDb.shutdown();
 	}
 
