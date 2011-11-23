734b8aa99a76028dd852dbf440d3f6c8fd6aea08
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-11-23 11:19:30 +0100

    [REFACTOR] Reorder specifications
    The specifications are reorded.

--- src/test/javascript/DataProviderSpec.js
+++ src/test/javascript/DataProviderSpec.js
@@ -3,22 +3,18 @@ describe("DataProvider", function(){
 		expect(DataProvider).toBeDefined();
 	});
 	
-	describe("factory",function(){
-		describe("mock", function(){
-			it("should exist", function(){
-				expect(DataProvider.mock).toBeDefined();
-			});
-			
-			it("should be instantiatable()", function(){
-				var provider = new DataProvider.mock();
-				expect(provider).toBeDefined();
-			});
+	describe("mock",function(){
+		it("should exist", function(){
+			expect(DataProvider.mock).toBeDefined();
 		});
-	});
-	
-	describe("methods of", function(){
-		var provider = new DataProvider.mock();
-		describe("mock", function(){
+		
+		it("should be instantiatable()", function(){
+			var provider = new DataProvider.mock();
+			expect(provider).toBeDefined();
+		});
+
+		describe("methods", function(){
+			var provider = new DataProvider.mock();
 			describe("pathOf", function(){
 				it("should exist", function(){
 					expect(provider.pathOf).toBeDefined();
